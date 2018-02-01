19e2c1ede0c70a2b9c8355cd69b21b660d6e9ee6
Thomas Widhalm <widhalmt@widhalm.or.at>
2018-02-01 11:49:09 +0100

    Changed the repository from Elastic Stack 5 to Elastic Stack 6.
    This helps with #2

--- modules.private/repos/manifests/elastic.pp
+++ modules.private/repos/manifests/elastic.pp
@@ -4,9 +4,9 @@ class repos::elastic {
 
       'redhat': {
 
-         yumrepo { 'elastic-5.x':
-            descr => 'Elastic repository for 5.x packages',
-            baseurl => 'https://artifacts.elastic.co/packages/5.x/yum',
+         yumrepo { 'elastic-6.x':
+            descr => 'Elastic repository for 6.x packages',
+            baseurl => 'https://artifacts.elastic.co/packages/6.x/yum',
             enabled => '1',
             gpgcheck => '1',
             gpgkey => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
